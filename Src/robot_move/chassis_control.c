c43a425f1855d3ee87d36fce2f3ee60729e3e923
kipper2 <18311390968@139.com>
2019-07-31 17:45:39 +0800

    Revert "Revert "闭位置环（完成）""
    This reverts commit 40468a332d447ff5dc44d965e0ba95b60e2bb638.

--- Src/robot_move/chassis_control.c
+++ Src/robot_move/chassis_control.c
@@ -11,7 +11,9 @@
 #define CHASSIS_MOTOR_RPM_TO_VECTOR_SEN 0.00005f
 #define AB 0.5f
 
-
+PidTypeDef auto_x;
+PidTypeDef auto_y;
+PidTypeDef auto_wz;
 extern uint8_t chassis_odom_pack_solve(
   float x,
   float y,
@@ -22,6 +24,9 @@ extern uint8_t chassis_odom_pack_solve(
   float gyro_z,
   float gyro_yaw);
 extern uint8_t CDC_Transmit_FS(uint8_t* Buf, uint16_t Len);
+extern int	result[2];
+location_t current;
+location_t target;
 
 
 
@@ -132,6 +137,7 @@ void chassis_auto_control(fp32 *vx_set, fp32 *vy_set, fp32 *wz_set, chassis_move
     {
         return;
     }
+<<<<<<< Updated upstream
 	//±ÕÎ»ÖÃ»·
    current.x = distance_x;
 	 current.y = distance_y;
@@ -140,6 +146,16 @@ void chassis_auto_control(fp32 *vx_set, fp32 *vy_set, fp32 *wz_set, chassis_move
 	*vx_set = PID_Calc_L(&PID_Lx, current.x, target.x);
 	*vy_set = PID_Calc_L(&PID_Ly, current.y, target.y);
 	*wz_set = PID_Calc_L(&PID_Lw, current.w, target.w);
+=======
+	
+	current.x = distance_x;
+	current.y = distance_y;
+	current.w = distance_wz;
+	PID_Calc_L(&auto_x, &auto_y, &target, &current);
+	*vx_set =result[0];
+	*vy_set =result[1];
+	*wz_set =PID_Calc(&auto_wz, current.w, target.w);
+>>>>>>> Stashed changes
 	
     return;
 }
